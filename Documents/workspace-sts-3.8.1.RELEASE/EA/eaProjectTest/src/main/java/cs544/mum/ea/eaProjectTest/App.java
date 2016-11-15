f943e1dde8f6348e9ed2cfdf7bdf5c322b09d26c
Santosh  Karki <Santosh  Karki@10.10.58.159>
2016-11-15 07:48:35 -0600

    Basic Configuration

---
+++ Documents/workspace-sts-3.8.1.RELEASE/EA/eaProjectTest/src/main/java/cs544/mum/ea/eaProjectTest/App.java
@@ -0,0 +1,13 @@
+package cs544.mum.ea.eaProjectTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Documents/workspace-sts-3.8.1.RELEASE/EA/eaProjectTest/src/main/java/cs544/mum/ea/eaProjectTest/model/Role.java
@@ -0,0 +1,6 @@
+package cs544.mum.ea.eaProjectTest.model;
+
+public enum Role {
+
+	 ROLE_ADMIN,ROLE_CUSTOMER
+}
