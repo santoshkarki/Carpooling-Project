c347eb2592c5a66b195f21d18343296a7367f58d
Santosh  Karki <Santosh  Karki@172.17.2.115>
2017-01-06 19:45:41 -0600

    hello

---
+++ workspace/WAA_LAB/HelloWorld/src/Hello.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class Hello {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		/*Scanner in = new Scanner(System.in);
+		System.out.println("Please Enter your name");
+		String name=in.nextLine();
+		System.out.println(name);
+		
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Enter your last name");
+		String lastName = bf.readLine();
+		System.out.println(lastName);*/
+
+	}
+
+}
+
